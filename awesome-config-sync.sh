103f4a41effe1a9a709bad83310575261be869e2
Luiz Salazar <luizsalazar87@gmail.com>
2023-03-14 02:59:37 -0500

    fix peek

--- awesome-config-sync.sh
+++ awesome-config-sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-rsync -r -t -v --progress -s $HOME/.config/awesome $HOME/darklinux-awesome/
-rsync -r -t -v --progress -s $HOME/.config/awesome $HOME/PKGBUILDS/darklinux-awesome-skel/etc/skel/.config/
+rsync -r -t -v --progress -s $HOME/.config/awesome $HOME/darklinux/darklinux-awesome/
+rsync -r -t -v --progress -s $HOME/.config/awesome $HOME/darklinux/darklinux-awesome-skel/etc/skel/.config/
