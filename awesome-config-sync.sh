bb61b669f59cfff9509dff1068294572759a385b
Luiz Salazar <luizsalazar87@gmail.com>
2023-09-24 01:04:28 -0500

    add chromebook backlight keybinds

--- awesome-config-sync.sh
+++ awesome-config-sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
+basedir=/mnt/SeagateNFS
 
-
-rsync -r -t -v --progress -s $HOME/.config/awesome $HOME/darklinux/darklinux-awesome/
-rsync -r -t -v --progress -s $HOME/.config/awesome $HOME/darklinux/darklinux-awesome-skel/etc/skel/.config/
+rsync -r -t -v --progress -s $HOME/.config/awesome $basedir/darklinux/darklinux-awesome/
+rsync -r -t -v --progress -s $HOME/.config/awesome $basedir/darklinux/darklinux-awesome-skel/etc/skel/.config/
